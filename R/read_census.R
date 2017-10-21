e7f6e50b17e5aae66c22c8e94c878c0fc37747db
GL-Li <liguanglai@gmail.com>
2017-10-20 20:08:47 -0400

    include SUMLEV and GEOCOMP as parameters in read_2010census

--- R/read_census.R
+++ R/read_census.R
@@ -15,6 +15,15 @@
 #' @param state vector of abbreviation of states, for example "IN" or c("MA", "RI").
 #' @param geoheaders vector of references of selected geographci headers to be included in the return.
 #' @param table_contents selected references of contents in census tables.
+#' @param summary_level select which summary level to keep, "*" to keep all. It takes strings
+#'        including "state", "county", "county_subdivision", "place", "tract", "block_group",
+#'        and "block" for the most common levels. It also take code for level. Search all codes with
+#'        \code{\link{search_sumlev}} for state files or \code{\link{search_sumlev_US}} for national files.
+#' @param geo_comp select which geographic component to keep, "*" to keep all.
+#'        Summary level of a state contains all geographic component. County
+#'        subdivision and higher level have all area ("00"), urban ("01"), and rural ("43").
+#'        census tract and lower level have no geographic component, all are "00". Browse
+#'        \code{\link{dict_geocomp}} for other geographic components.
 #' @param show_progress show progress of file reading if TRUE. Turn off if FALSE, which
 #'     is useful in RMarkdown output.
 #'
@@ -24,23 +33,39 @@
 #'
 #' @examples
 #' \dontrun{
-#' # read Rhode Island and Massachusette summary data
+#' # read Rhode Island and Massachusette summary data at block level
 #' ri <- read_2010census("your_local_path_to_2010_census", c("RI", "MA"),
-#'                        c("GEOCOMP", "NAME", "SUMLEV"),
-#'                        c("PCT012F139", "PCT012F138", "P0150008", "H0070016"))
+#'                        geoheaders = c("NAME", "INTPTLON", "INTPTLAT"),
+#'                        table_contents = c("PCT012F139", "P0150008"),
+#'                        summary_level = "block")
 #'
 #' # read the national summary data
 #' us <- read_2010census("your_local_path_to_2010_census", "US",
-#'                        c("STATE", "COUNTY", "COUSUB", "GEOCOMP", "NAME", "SUMLEV"),
-#'                        c("PCT012F139", "PCT012F138", "P0150008", "H0070016"))
+#'                        geoheaders = c("STATE", "COUNTY", "COUSUB", "NAME"),
+#'                        table_contents = c("PCT012F139", "P0150008"),
+#'                        summary_level = "county_subdivision")
 #' }
 #'
 #' @export
 #'
 #'
 
-read_2010census <- function(path_to_census, state, geoheaders ,
-                            table_contents, show_progress = TRUE){
+read_2010census <- function(path_to_census, state, geoheaders, table_contents,
+                            summary_level = "*", geo_comp = "*",
+                            show_progress = TRUE){
+    # switch summary level to code
+    if (summary_level %in% c("state", "county", "county_subdivision", "place",
+                             "tract", "block_group", "block")){
+        summary_level <- switch(summary_level,
+                                "state" = "040",
+                                "county" = "050",
+                                "county_subdivision" = "060",
+                                "place" = "160",
+                                "tract" = "140",
+                                "block_group" = "150",
+                                "block" = "100")
+    }
+
     lst <- list()
     for (st in toupper(state)){
         # add st to geoheaders as there are multiple state
@@ -49,7 +74,14 @@ read_2010census <- function(path_to_census, state, geoheaders ,
             .[, state := st]
         data <- read_2010tablecontents(path_to_census, st, table_contents,
                                        show_progress = show_progress)
-        census <- geo[data]
+        census <- geo[data] %>%
+            .[SUMLEV %like% summary_level & GEOCOMP %like% geo_comp] %>%
+            .[, LOGRECNO := NULL]
+
+        # delete SUMLEV and GEOCOMP if already selected
+        if (summary_level != "*") census[, SUMLEV := NULL]
+        if (geo_comp != "*") census[, GEOCOMP := NULL]
+
 
         lst[[st]] = census
     }
