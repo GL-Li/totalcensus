81353b1d40ee95589f3ca1a42e499e67e7258592
Robb Fitzsimmons <robb@linnaean.co>
2019-12-20 16:51:05 -0600

    Update with 2018 ACS 5-year

--- R/data_acs.R
+++ R/data_acs.R
@@ -1,3 +1,31 @@
+# lookup_acs5year_2018 =========================================================
+#' ACS 5-year 2018 file segment and table lookup data
+#'
+#' @docType data
+#'
+#' @format A data.table with 26996 rows and 7 variables
+#' \describe{
+#'   \item{file_segment}{sequence number of segment data files, from "0001" to "0122"}
+#'   \item{table_content}{description of columns in a table}
+#'   \item{reference}{reference of the table content, such as "B01001_002". The reference
+#'         is used to extract data of table content.}
+#'   \item{restriction}{restrictions applied the the table_content}
+#'   \item{table_number}{table number such as "B01001"}
+#'   \item{table_name}{description of table. A table has multiple columns (table_content)}
+#'   \item{universe}{the universe of the data}
+#' }
+#'
+#' @keywords datasets
+#'
+#' @source Check for each year of ACS 1-year and 5-year
+#' \href{https://www.census.gov/programs-surveys/acs/technical-documentation/summary-file-documentation.2018.html}{Sequence Number/Table Number Lookup File}.
+#'
+
+"lookup_acs5year_2018"
+
+
+
+
 # lookup_acs5year_2017 =========================================================
 #' ACS 5-year 2017 file segment and table lookup data
 #'
--- R/search_census_and_acs.R
+++ R/search_census_and_acs.R
@@ -705,6 +705,7 @@ generate_acs5_tablecontents_ <- function(){
     acs5_2015 <- modify_lookup_table_(5, 2015)
     acs5_2016 <- modify_lookup_table_(5, 2016)
     acs5_2017 <- modify_lookup_table_(5, 2017)
+    acs5_2018 <- modify_lookup_table_(5, 2018)
 
     dict_acs_tablecontent <- reduce(list(acs5_2009,
                                          acs5_2010,
@@ -714,7 +715,8 @@ generate_acs5_tablecontents_ <- function(){
                                          acs5_2014,
                                          acs5_2015,
                                          acs5_2016,
-                                         acs5_2017),
+                                         acs5_2017,
+                                         acs5_2018),
                                     merge, by = "reference", all = TRUE) %>%
 
         # add the following lines for year since 2013
@@ -733,10 +735,13 @@ generate_acs5_tablecontents_ <- function(){
         .[!is.na(acs5_2017), ":=" (table_content = content_acs5_2017,
                                    table_name = name_acs5_2017,
                                    universe = universe_acs5_2017)] %>%
+        .[!is.na(acs5_2018), ":=" (table_content = content_acs5_2018,
+                                   table_name = name_acs5_2018,
+                                   universe = universe_acs5_2018)] %>%
 
         # include all years and surveys
         .[, .(reference, table_content, table_name,
-              acs5_2017, acs5_2016, acs5_2015, acs5_2014,
+              acs5_2018, acs5_2017, acs5_2016, acs5_2015, acs5_2014,
               acs5_2013, acs5_2012, acs5_2011,
               acs5_2010, acs5_2009,
               universe)]
