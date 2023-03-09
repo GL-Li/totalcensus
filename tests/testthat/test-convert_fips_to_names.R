8320c096e002e03937d0fd839dcc76be6b8400cf
GL-Li <liguanglai@gmail.com>
2023-03-09 07:36:31 -0500

    update test for convert_fips_and_names

--- tests/testthat/test-convert_fips_to_names.R
+++ tests/testthat/test-convert_fips_to_names.R
@@ -1,3 +1,5 @@
 test_that("convert_fips_to_names", {
   expect_equal(convert_fips_to_names(c("11", "44")), c("DC", "RI"))
+  expect_equal(convert_fips_to_names(c("001", "013"), states = c("RI", "MA"), geo_header = "COUNTY"),
+               c("Bristol County", "Hampden County"))
 })
